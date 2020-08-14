b9b08c8667d4f54414fe068f9092b8ccc9ddcea9
François Gindraud <francois.gindraud@gmail.com>
2020-08-15 00:59:13 +0200

    intrusive list : move to refcell like api
    Use RefCell-like model to handle lack of statically expressible ownership model.
    
    Make prev/next Cell<ptr> to avoid creating temporary &mut RawLink which may alias (probably UB even if accessing exclusive fields).
    
    Added a Cell<i32> for the reference count ; Cell as it can be modified from shared references (iterating).
    Link<T> contains the UnsafeCell for T whose access is controlled by the ref count.
    
    The Ref count is in RawLink as it is also used to track usage and prevent drop of referenced links, which is impossible to prevent due to lack of typeing ownership.
    
    Also add a const field with link type for detecting iteration end.
    It fits inside space left by ref_count (i32, aligned to i64).
    Sadly this means RawLink now takes 3 words, but this is not avoidable.

--- src/intrusive_chain.rs
+++ src/intrusive_chain.rs
@@ -1,50 +1,87 @@
+//! This module implements a non-owning intrusive chain (list) with a Safe API.
+//!
+//! To have a safe API, we must prevent aliasing access to values and ensure values are alive during accesses.
+//! Contrary to an owning intrusive list, we cannot statically link lifetime of elements to the list head.
+//! An example is [`Drop::drop`] which gets `&mut` access, and could happen at any time.
+//!
+//! Thus the strategy is for each link to act like a [`core::cell::RefCell`], dynamically tracking its reference state.
+//! Safety requires panicking during drop if the link was referenced.
+
+use core::cell::{Cell, UnsafeCell};
 use core::fmt;
-use core::hint::unreachable_unchecked;
 use core::marker::{PhantomData, PhantomPinned};
-use core::ops::Deref;
 use core::pin::Pin;
-use core::ptr::NonNull;
+use std::error::Error;
 
 /// Basic link that can **safely** form a doubly-linked circular chain with other pinned instances.
-/// Conceptually, this struct owns the _participation in a chain_ if linked.
+/// Conceptually, this struct owns the _participation in a chain_.
 /// It only cares about creating and maintaining the chain pointer structure, and provides no iteration semantics on its own.
 ///
-/// It is either unlinked, or in a chain (maybe singleton with only self).
-/// If in a chain: `self == self.prev.next == self.next.prev`.
-/// It can only be put in a chain if it is pinned, thus all chain members are pinned.
+/// A link starts unlinked (`self.prev == self.next == null`).
+/// **After being pinned** it can be placed _in a chain_ (maybe singleton with only self).
+/// If in a chain, `self == self.prev.next == self.next.prev`, and all chain members are pinned.
 /// It can only be in one chain.
 ///
-/// This struct is [`Unpin`], and all link operations require [`Pin`] references.
+/// This struct is [`!Unpin`](Unpin), and all link operations require [`Pin`] references.
 /// This guarantuees that pointers to links are stable (no move allowed).
+///
+/// All inner field are **not pin structural**.
 struct RawLink {
-    /// Not pin structural.
-    state: RawLinkState,
+    /// `Pin<&RawLink>` if not null.
+    prev: Cell<*const RawLink>,
+    /// `Pin<&RawLink>` if not null.
+    next: Cell<*const RawLink>,
+    /// `>0` for shared references, or special values [`UNREFERENCED`] or [`EXCLUSIVE_REFERENCE`].
+    references: Cell<i32>,
+    link_type: RawLinkType,
     _pin: PhantomPinned,
 }
 
-enum RawLinkState {
-    /// Each pointer is a `Pin<&mut RawLink>`.
-    InChain {
-        prev: NonNull<RawLink>,
-        next: NonNull<RawLink>,
-    },
-    Unlinked,
+const UNREFERENCED: i32 = 0;
+const EXCLUSIVE_REFERENCE: i32 = -1;
+
+enum RawLinkType {
+    Chain,
+    Link,
 }
 
 impl RawLink {
-    /// New unlinked raw link.
-    fn new() -> Self {
+    /// Create a new unlinked raw link.
+    fn new(link_type: RawLinkType) -> Self {
         RawLink {
-            state: RawLinkState::Unlinked,
+            prev: Cell::new(core::ptr::null()),
+            next: Cell::new(core::ptr::null()),
+            references: Cell::new(UNREFERENCED),
+            link_type,
             _pin: PhantomPinned,
         }
     }
 
     fn is_linked(&self) -> bool {
-        match self.state {
-            RawLinkState::Unlinked => false,
-            _ => true,
-        }
+        !self.next.get().is_null()
+    }
+
+    // TODO replace (linked | unlinked) by (unpinned | singleton | chained) ?
+    // TODO determine model to use for insert/unlink (singleton, of unpinned)
+    fn is_pinned(&self) -> bool {
+        !self.next.get().is_null()
+    }
+    fn is_singleton(&self) -> bool {
+        self.next.get() == self
+    }
+
+    fn try_borrow<'l>(&'l self) -> Result<RawLinkBorrow<'l>, BorrowError> {
+        RawLinkBorrow::new(self)
+    }
+    fn borrow<'l>(&'l self) -> RawLinkBorrow<'l> {
+        self.try_borrow().unwrap()
+    }
+
+    fn try_borrow_mut<'l>(&'l self) -> Result<RawLinkBorrowMut<'l>, BorrowMutError> {
+        RawLinkBorrowMut::new(self)
+    }
+    fn borrow_mut<'l>(&'l self) -> RawLinkBorrowMut<'l> {
+        self.try_borrow_mut().unwrap()
     }
 
     /// If self is linked:
@@ -52,30 +89,22 @@ impl RawLink {
     /// /--p->-self->-n--\ -> /--p->-n--\ + unlinked self
     /// \-------<--------/    \----<----/
     /// ```
-    fn unlink(self: Pin<&mut Self>) {
-        unsafe {
-            // Inner content is not pin-structural
-            let self_mut = self.get_unchecked_mut();
-            if let RawLinkState::InChain {
-                prev: mut p_ptr,
-                next: mut n_ptr,
-            } = self_mut.state
-            {
+    fn unlink(&self) {
+        if self.is_linked() {
+            let p_ptr = self.prev.get();
+            let n_ptr = self.next.get();
+            // if p == n == self: singleton, no need to fix p.next / n.prev
+            if p_ptr != self {
+                // SAFETY:
                 // self in a chain => p & n are pinned raw links in a chain.
                 // Set p.next = n & n.prev = p
-                // if p == n == self: singleton, no need to change pointers.
-                if p_ptr != NonNull::new_unchecked(self_mut) {
-                    match &mut p_ptr.as_mut().state {
-                        RawLinkState::InChain { next, .. } => *next = n_ptr,
-                        _ => unreachable_unchecked(),
-                    }
-                    match &mut n_ptr.as_mut().state {
-                        RawLinkState::InChain { prev, .. } => *prev = p_ptr,
-                        _ => unreachable_unchecked(),
-                    }
+                unsafe {
+                    (*p_ptr).next.set(n_ptr);
+                    (*n_ptr).prev.set(p_ptr);
                 }
-                self_mut.state = RawLinkState::Unlinked;
             }
+            self.prev.set(core::ptr::null());
+            self.next.set(core::ptr::null())
         }
     }
 
@@ -84,43 +113,24 @@ impl RawLink {
     /// \------<-----/                                     \---------<----------/
     /// ```
     /// Unlinks `other` if it is linked.
-    fn insert_prev(self: Pin<&mut Self>, mut other: Pin<&mut Self>) {
-        other.as_mut().unlink();
-        // SAFETY: we can chain them as they are pinned.
-        unsafe {
-            let self_mut = self.get_unchecked_mut();
-            let other = other.get_unchecked_mut();
-            let self_ptr = NonNull::new_unchecked(self_mut);
-            let other_ptr = NonNull::new_unchecked(other);
+    fn insert_prev(self: Pin<&Self>, other: Pin<&Self>) {
+        other.unlink();
 
-            match &mut self_mut.state {
-                RawLinkState::InChain {
-                    prev: self_prev, ..
-                } => {
-                    // p & self in a chain
-                    let mut p_ptr = *self_prev;
-                    match &mut p_ptr.as_mut().state {
-                        RawLinkState::InChain { next, .. } => *next = other_ptr,
-                        _ => unreachable_unchecked(),
-                    }
-                    *self_prev = other_ptr;
-                    other.state = RawLinkState::InChain {
-                        prev: p_ptr,
-                        next: self_ptr,
-                    }
-                }
-                self_state => {
-                    // self unlinked
-                    *self_state = RawLinkState::InChain {
-                        prev: other_ptr,
-                        next: other_ptr,
-                    };
-                    other.state = RawLinkState::InChain {
-                        prev: self_ptr,
-                        next: self_ptr,
-                    }
-                }
-            }
+        let self_ptr: *const RawLink = self.get_ref();
+        let other_ptr: *const RawLink = other.get_ref();
+
+        if self.is_linked() {
+            let p_ptr = self.prev.get();
+            // SAFETY : self in chain => p_ptr is valid and pinned
+            unsafe { (*p_ptr).next.set(other_ptr) };
+            self.prev.set(other_ptr);
+            other.prev.set(p_ptr);
+            other.next.set(self_ptr)
+        } else {
+            self.prev.set(other_ptr);
+            self.next.set(other_ptr);
+            other.prev.set(self_ptr);
+            other.next.set(self_ptr)
         }
     }
 }
@@ -128,11 +138,88 @@ impl RawLink {
 /// Disconnect from any chain on destruction
 /// Part of SAFETY ; being in a chain => pinned => destructor will run before repurposing memory.
 /// Thus pointers to self in neighbouring links are valid (removed before memory is repurposed).
+///
+/// Panics if the link is referenced, as drop is equivalent to getting exclusive access (`&mut`).
 impl Drop for RawLink {
     fn drop(&mut self) {
-        unsafe { Pin::new_unchecked(self).unlink() }
+        // Drop has borrowed self mutably.
+        // Check with dynamic borrow that it could actually do so before continuing.
+        // If not, panic as there is no way to indicate an error of stop drop().
+        let _mut_guard = self.borrow_mut();
+        self.unlink()
     }
 }
+
+/// Shared immutable borrow guard.
+///
+/// Holds a `+1` value in the reference count sum.
+/// In case of overflow, use wraparound which will cause next borrow to fail (no UB).
+struct RawLinkBorrow<'l> {
+    link: &'l RawLink,
+}
+
+impl<'l> RawLinkBorrow<'l> {
+    fn new(link: &'l RawLink) -> Result<Self, BorrowError> {
+        let ref_count = link.references.get();
+        if ref_count >= 0 {
+            link.references.set(ref_count.wrapping_add(1));
+            Ok(RawLinkBorrow { link })
+        } else {
+            Err(BorrowError)
+        }
+    }
+}
+
+impl<'l> Drop for RawLinkBorrow<'l> {
+    fn drop(&mut self) {
+        let ref_count = self.link.references.get();
+        self.link.references.set(ref_count.wrapping_sub(1))
+    }
+}
+
+/// Represents failure to borrow a link (shared immutable borrow).
+#[derive(Debug)]
+pub struct BorrowError;
+impl fmt::Display for BorrowError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        "Cannot borrow link (shared)".fmt(f)
+    }
+}
+impl Error for BorrowError {}
+
+/// Exclusive mutable borrow guard.
+///
+/// Only one can exist at anytime.
+struct RawLinkBorrowMut<'l> {
+    link: &'l RawLink,
+}
+
+impl<'l> RawLinkBorrowMut<'l> {
+    fn new(link: &'l RawLink) -> Result<Self, BorrowMutError> {
+        if link.references.get() == UNREFERENCED {
+            link.references.set(EXCLUSIVE_REFERENCE);
+            Ok(RawLinkBorrowMut { link })
+        } else {
+            Err(BorrowMutError)
+        }
+    }
+}
+
+impl<'l> Drop for RawLinkBorrowMut<'l> {
+    fn drop(&mut self) {
+        self.link.references.set(UNREFERENCED)
+    }
+}
+
+/// Represents failure to borrow a link (exclusive mutable borrow).
+#[derive(Debug)]
+pub struct BorrowMutError;
+impl fmt::Display for BorrowMutError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        "Cannot borrow link (exclusive)".fmt(f)
+    }
+}
+impl Error for BorrowMutError {}
 
 /// A value that can be threaded in a chain.
 /// Each [`Link`] can be in only one [`Chain`] at a time, and only once.
@@ -140,28 +227,15 @@ impl Drop for RawLink {
 pub struct Link<T: ?Sized> {
     /// RawLink is first element with repr(C) to allow static casting between raw <-> self
     raw: RawLink,
-    value: T,
+    value: UnsafeCell<T>,
 }
 
 impl<T> Link<T> {
     pub fn new(value: T) -> Link<T> {
         Link {
-            raw: RawLink::new(),
-            value,
+            raw: RawLink::new(RawLinkType::Link),
+            value: UnsafeCell::new(value),
         }
-    }
-}
-
-impl<T: ?Sized> Deref for Link<T> {
-    type Target = T;
-    fn deref(&self) -> &T {
-        &self.value
-    }
-}
-
-impl<T: ?Sized + fmt::Debug> fmt::Debug for Link<T> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        self.deref().fmt(f)
     }
 }
 
@@ -175,12 +249,12 @@ pub struct Chain<T: ?Sized> {
 impl<T> Chain<T> {
     pub fn new() -> Self {
         Chain {
-            raw: RawLink::new(),
+            raw: RawLink::new(RawLinkType::Chain),
             _marker: PhantomData,
         }
     }
 
-    pub fn insert(chain: Pin<&mut Self>, link: Pin<&mut Link<T>>) {
+    pub fn push_front(chain: Pin<&mut Self>, link: Pin<&mut Link<T>>) {
         unimplemented!()
     }
 }
@@ -189,19 +263,19 @@ impl<T> Chain<T> {
 fn test_raw() {
     assert_eq!(
         core::mem::size_of::<RawLink>(),
-        2 * core::mem::size_of::<*const ()>()
+        3 * core::mem::size_of::<*const ()>()
     );
-    let mut link0 = Box::pin(RawLink::new());
-    let mut link1 = Box::pin(RawLink::new());
+    let link0 = Box::pin(RawLink::new(RawLinkType::Link));
+    let link1 = Box::pin(RawLink::new(RawLinkType::Link));
     assert!(!link0.is_linked());
     assert!(!link1.is_linked());
-    link0.as_mut().insert_prev(link1.as_mut());
+    link0.as_ref().insert_prev(link1.as_ref());
     assert!(link0.is_linked());
     assert!(link1.is_linked());
-    link1.as_mut().unlink();
+    link1.as_ref().unlink();
     assert!(link0.is_linked());
     assert!(!link1.is_linked());
-    link1.as_mut().insert_prev(link0.as_mut());
+    link1.as_ref().insert_prev(link0.as_ref());
     assert!(link0.is_linked());
     assert!(link1.is_linked());
     drop(link0);
